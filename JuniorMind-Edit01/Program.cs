0f26d14b7fabfec129d489dbf5014dc544eacba3
Sergiu95 <Sergiu95@SERGIU>
2020-10-21 12:28:27 +0300

    last updates

--- JuniorMind-Edit01/Program.cs
+++ JuniorMind-Edit01/Program.cs
@@ -7,6 +7,8 @@ namespace JuniorMind_Edit01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New commit");
+            Console.Read();
         }
     }
 }
