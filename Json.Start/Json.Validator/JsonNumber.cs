133ec9740cb3e7ec1e4357a35d519597d0be559b
Sergiu95 <Sergiu95@SERGIU>
2020-10-29 17:36:07 +0200

    CW()Removed

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -19,7 +19,6 @@ namespace Json
         {
             if (input.Contains("e", StringComparison.CurrentCultureIgnoreCase))
             {
-                Console.WriteLine("");
                 int positionOfExponent = input.IndexOf('e'.ToString(), StringComparison.CurrentCultureIgnoreCase);
                 int sameExponentPosition = input.LastIndexOf('e'.ToString(), StringComparison.CurrentCultureIgnoreCase);
                 bool onlyOneExponent = positionOfExponent == sameExponentPosition;
