b36a1b9e0bfd9ff1b999fa6206ed47cde47eabe1
Sergiu95 <Sergiu95@SERGIU>
2020-10-29 14:10:25 +0200

    TryParse used to validate 6 tests

--- Json.Start/Json.Facts/JsonNumberFacts.cs
+++ Json.Start/Json.Facts/JsonNumberFacts.cs
@@ -6,37 +6,37 @@ namespace Json.Facts
 {
     public class JsonNumberFacts
     {
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void CanBeZero()
         {
             Assert.True(IsJsonNumber("0"));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void DoesNotContainLetters()
         {
             Assert.False(IsJsonNumber("a"));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void CanHaveASingleDigit()
         {
             Assert.True(IsJsonNumber("7"));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void CanHaveMultipleDigits()
         {
             Assert.True(IsJsonNumber("70"));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void IsNotNull()
         {
             Assert.False(IsJsonNumber(null));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void IsNotAnEmptyString()
         {
             Assert.False(IsJsonNumber(string.Empty));
--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,7 +6,7 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            throw new NotImplementedException();
+            return double.TryParse(input, out double number);
         }
     }
 }
