b4132a1a3dd2af7b63923302073657e80c2680e1
Sergiu95 <Sergiu95@SERGIU>
2020-10-30 14:39:50 +0200

    Program validates correct fractions

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -4,6 +4,8 @@ namespace Json
 {
     public static class JsonNumber
     {
+        const string Digits = "0123456789";
+
         public static bool IsJsonNumber(string input)
             {
             if (input == null || input.Length < 1)
@@ -11,12 +13,17 @@ namespace Json
                 return false;
             }
 
-            return IsNumber(input);
+            return IsCorrectFraction(input) && IsNumber(input);
         }
 
         private static bool IsNumber(string input)
         {
-          for (int i = 0; i < input.Length; i++)
+          if (!IsValidFirstDigit(input))
+            {
+                return false;
+            }
+
+          for (int i = 1; i < input.Length; i++)
             {
                 if (!IsCorrectDigit(input[i], i))
                 {
@@ -24,13 +31,29 @@ namespace Json
                 }
             }
 
-          return input[0] != '0';
+          return true;
+        }
+
+        private static bool IsValidFirstDigit(string input)
+        {
+            if (input.Contains('.'))
+            {
+                return (Digits + "+-").Contains(input[0]);
+            }
+
+            string digitsWithoutZero = Digits.Substring(1) + "+-";
+            return input.Length > 1 ? digitsWithoutZero.Contains(input[0]) : Digits.Contains(input[0]);
+        }
+
+        private static bool IsCorrectFraction(string input)
+        {
+            return input.IndexOf('.') == input.LastIndexOf('.') && input.IndexOf('.') != 0
+                && input.IndexOf('.') != input.Length - 1;
         }
 
         private static bool IsCorrectDigit(char digit, int inputIndex)
-        {
-            const string Digits = "0123456789";
-            return inputIndex == 0 ? (Digits + "+-").Contains(digit) : Digits.Contains(digit);
-        }
+            {
+            return (Digits + '.').Contains(digit);
+            }
     }
 }
