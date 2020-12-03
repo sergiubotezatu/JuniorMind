6a17f94926e7aa93f3501128a3cd681212300370
Sergiu95 <Sergiu95@SERGIU>
2020-12-03 16:43:05 +0200

    Many class added

---
+++ Choice/ChoiceLibrary/Many.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterFace;
+
+namespace ChoiceLibrary
+{
+    public class Many :IPattern
+    {
+        IPattern pattern;
+
+        public Many(IPattern Pattern)
+        {
+            this.pattern = Pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return string.IsNullOrEmpty(text)
+                ? new Match(true, text)
+                : GetRedundantText(text);
+        }
+
+        private Match GetRedundantText(string text)
+        {
+            IMatch result = new Match(true, text);
+            foreach (char element in text)
+            {
+                result = this.pattern.Match(result.RemainingText());
+                if (!result.Success())
+                {
+                    return new Match(true, result.RemainingText());
+                }
+            }
+
+            return new Match(true, string.Empty);
+        }
+    }
+}
---
+++ Choice/IPatternFacts/ManyFacts.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ChoiceLibrary;
+using InterFace;
+using Range = InterFace.Range;
+
+namespace ChoiceFacts
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void ReturnsNullWhenInputIsNull()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.True(a.Match(null).Success());
+            Assert.True(a.Match(null).RemainingText() == null);
+        }
+
+        [Fact]
+        public void ReturnsEmptyStringWhenInputIsEmpty()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.True(a.Match("").Success());
+            Assert.True(a.Match("").RemainingText() == "");
+        }
+
+        [Fact]
+        public void ReturnsInputIfPatternIsNotLocatedAtTheStart()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.True(a.Match("ba").Success());
+            Assert.True(a.Match("ba").RemainingText() == "ba");
+        }
+
+        [Fact]
+        public void ReturnsEmptyStringIfInputAndPatternAreTheSame()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.True(a.Match("a").Success());
+            Assert.True(a.Match("a").RemainingText() == "");
+        }
+
+        [Fact]
+        public void ReturnsInputExceptIdentifiedPattern()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.True(a.Match("abc").Success());
+            Assert.True(a.Match("abc").RemainingText() == "bc");
+        }
+
+        [Fact]
+        public void ReturnsInputExceptIdentifiedRepeatedPattern()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.True(a.Match("aaaaabc").Success());
+            Assert.True(a.Match("aaaaabc").RemainingText() == "bc");
+        }
+
+        [Theory]
+        [InlineData(null, true, null)]
+        [InlineData("", true, "")]
+        [InlineData("ab", true, "ab")]
+        [InlineData("a1b", true, "a1b")]
+        [InlineData("1234", true, "")]
+        [InlineData("1546ab1", true, "ab1")]
+        public void WorksWithRangePatterns(string text, bool True, string expected)
+        {
+            var digits = new Many(new Range('0', '9'));
+            bool actual = digits.Match(text).Success();
+            string result = digits.Match(text).RemainingText();
+            Assert.True(actual == True);
+            Assert.True(result == expected);
+        }
+    }
+}
