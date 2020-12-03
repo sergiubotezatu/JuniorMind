aa827c5dcef6d3d94f65673b23868e6c7249e70b
Sergiu95 <Sergiu95@SERGIU>
2020-12-03 12:57:37 +0200

    Any class added

---
+++ Choice/ChoiceLibrary/Any.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterFace;
+
+namespace ChoiceLibrary
+{
+    public class Any : IPattern
+    {
+        string accepted;
+
+        public Any(string Accepted)
+        {
+            this.accepted = Accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+
+        }
+    }
+}
---
+++ Choice/IPatternFacts/AnyTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChoiceLibrary;
+using InterFace;
+using Xunit;
+
+namespace ChoiceFacts
+{
+    public class AnyTests
+    {
+        [Fact]
+        public void InvalidatesNullStrings()
+        {
+            var a = new Any("aA");
+            Assert.False(a.Match(null).Success());
+            Assert.True(a.Match(null).RemainingText() == null);
+        }
+
+        [Fact]
+        public void InvalidatesEmptyStrings()
+        {
+            var a = new Any("aA");
+            Assert.False(a.Match("").Success());
+            Assert.True(a.Match("").RemainingText() == "");
+        }
+
+        [Fact]
+        public void ValidatesFirstCharWhenIsTheSameAsFirstInAny()
+        {
+            var a = new Any("aA");
+            Assert.True(a.Match("ab").Success());
+            Assert.True(a.Match("").RemainingText() == "b");
+        }
+
+        [Fact]
+        public void ValidatesFirstCharWhenIsTheSameAsSecondInAny()
+        {
+            var a = new Any("aA");
+            Assert.True(a.Match("Ab").Success());
+            Assert.True(a.Match("").RemainingText() == "b");
+        }
+
+        [Fact]
+        public void InvalidatesFirstCharWhenItIsnotFoundInAny()
+        {
+            var a = new Any("aA");
+            Assert.False(a.Match("bA").Success());
+            Assert.True(a.Match("").RemainingText() == "bA");
+        }
+    }
+}
