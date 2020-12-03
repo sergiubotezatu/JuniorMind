748e8b18f20ad17416fe04acbf7c68c564c689a7
Sergiu95 <Sergiu95@SERGIU>
2020-12-03 17:29:46 +0200

    OptionalFacts added

---
+++ Choice/ChoiceLibrary/Optional.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterFace;
+
+namespace ChoiceLibrary
+{
+    public class Optional : IPattern
+    {
+        IPattern pattern;
+
+        public Optional(IPattern Pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return new Match(true, text);
+        }
+    }
+}
---
+++ Choice/IPatternFacts/OptionalFacts.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterFace;
+using Xunit;
+using ChoiceLibrary;
+
+namespace ChoiceFacts
+{
+    public class OptionalFacts
+    {
+        [Fact]
+        public void ReturnsNullWhenInputIsNull()
+        {
+            var a = new Optional(new Character('a'));
+
+            Assert.True(a.Match(null).Success());
+            Assert.True(a.Match(null).RemainingText() == null);
+        }
+
+        [Fact]
+        public void ReturnsEmptyStringWhenInputIsEmpty()
+        {
+            var a = new Optional(new Character('a'));
+
+            Assert.True(a.Match(string.Empty).Success());
+            Assert.True(a.Match(string.Empty).RemainingText() == string.Empty);
+        }
+
+        [Fact]
+        public void ReturnsInputIfPatternIsNotLocatedAtTheStart()
+        {
+            var a = new Optional(new Character('a'));
+
+            Assert.True(a.Match("bc").Success());
+            Assert.True(a.Match("bc").RemainingText() == "bc");
+        }
+
+        [Fact]
+        public void ReturnsEmptyStringIfInputAndPatternAreTheSame()
+        {
+            var a = new Optional(new Character('a'));
+
+            Assert.True(a.Match("a").Success());
+            Assert.True(a.Match("a").RemainingText() == string.Empty);
+        }
+
+        [Fact]
+        public void ReturnsInputExceptIdentifiedPattern()
+        {
+            var a = new Optional(new Character('a'));
+
+            Assert.True(a.Match("abc").Success());
+            Assert.True(a.Match("abc").RemainingText() == "bc");
+        }
+
+        [Fact]
+        public void ReturnsInputExceptFirstIdentifiedPattern()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.True(a.Match("aaabc").Success());
+            Assert.True(a.Match("aaabc").RemainingText() == "aabc");
+        }
+    }
+}
