a855e8298829f5b726d0c5d260a081b9bff3624a
Sergiu95 <Sergiu95@SERGIU>
2021-01-25 15:22:19 +0200

    number Lastupdate

--- Choice/ChoiceLibrary/Number.cs
+++ Choice/ChoiceLibrary/Number.cs
@@ -12,18 +12,16 @@ namespace ChoiceLibrary
 
         public Number()
         {
-            var digits = new OneOrMore(new Range('1', '9'));
-            var zero = new Character('0');
+            var digits = new OneOrMore(new Range('0', '9'));
+            var zero = new Character('0'); 
             var minus = new Optional(new Character('-'));
             var decimalDot = new Character('.');
             var signs = new Optional(new Any("+-"));
             var exponential = new Any("eE");
-            var minusZero = new Sequence(minus, zero);
-            var integer = new Sequence(minus, digits, new Many(new Choice(zero, digits)));
-            var decimalPart = new Sequence(new Range('0', '9'), new Many(zero), new List(digits, new Many(zero)));
-            var fractional = new Sequence(decimalDot, decimalPart);
-            var exponent = new Sequence(exponential, signs, digits, new Many(new Choice(digits, zero)));
-            this.pattern = new Sequence(new Choice(integer, minusZero), new Optional(fractional), new Optional(exponent));
+            var integer = new Sequence(minus, new Choice(zero, digits));
+            var fractional = new Sequence(decimalDot, digits);
+            var exponent = new Sequence(exponential, signs, new Range('1','9'), new Optional(digits));
+            this.pattern = new Sequence(integer, new Optional(fractional), new Optional(exponent));
         }
 
         public IMatch Match(string text)
