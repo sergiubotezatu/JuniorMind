caad4ed171c332d93f0d465c73058a685949cb2e
Sergiu95 <Sergiu95@SERGIU>
2021-03-10 17:29:25 +0200

    IList<T> implementation

--- ArrayLibrary/ArrayLibrary/SortedList.cs
+++ ArrayLibrary/ArrayLibrary/SortedList.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ArrayLibrary
 {
-    public class SortedList<T>
+    public class SortedList<T> : IList<T>
         where T : IComparable<T>
     {
         private readonly ArrayLibrary.List<T> initList;
@@ -16,6 +17,8 @@ namespace ArrayLibrary
         }
 
         public int Count { get => this.initList.Count; }
+
+        public bool IsReadOnly => throw new NotImplementedException();
 
         public T this[int index]
         {
@@ -31,39 +34,31 @@ namespace ArrayLibrary
             }
         }
 
-        public void Add(T element)
+        public void Add(T item)
         {
-            this.initList.Add(element);
+            this.initList.Add(item);
             if (Count > 1)
             {
-                SortWhileAdding(element);
+                SortWhileAdding(item);
             }
         }
 
-        public void Insert(int index, T element)
+        public void Insert(int index, T item)
         {
-            if (IsInBetween(element, index))
+            if (IsInBetween(item, index))
             {
-                this.initList.Insert(index, element);
+                this.initList.Insert(index, item);
             }
         }
 
-        public bool Contains(T element)
+        public bool Contains(T item)
         {
-            return !IndexOf(element).Equals(-1);
+            return this.initList.Contains(item);
         }
 
-        public int IndexOf(T element)
+        public int IndexOf(T item)
         {
-            for (int i = 0; i < Count; i++)
-            {
-                if (this.initList[i].Equals(element))
-                {
-                    return i;
-                }
-            }
-
-            return -1;
+            return this.initList.IndexOf(item);
         }
 
         public void Clear()
@@ -71,7 +66,7 @@ namespace ArrayLibrary
             this.initList.Clear();
         }
 
-        public void Remove(T element)
+        public void Delete(T element)
         {
             this.initList.Remove(element);
         }
@@ -79,6 +74,60 @@ namespace ArrayLibrary
         public void RemoveAt(int index)
         {
             this.initList.RemoveAt(index);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                return;
+            }
+
+            int minimumLength = array.Length + this.Count;
+            if (array.Length >= minimumLength)
+           {
+                for (int i = 0; i < this.Count; i++)
+                {
+                    array[arrayIndex] = this.initList[i];
+                    arrayIndex++;
+                }
+           }
+        }
+
+        public bool Remove(T item)
+        {
+            int itemPos = IndexOf(item);
+            int newCount = this.Count - 1;
+            if (itemPos == -1)
+            {
+                return false;
+            }
+
+            if (this.Count == 1)
+            {
+                Remove(item);
+                return this.Count == 0;
+            }
+
+            Remove(item);
+            T replacer = GetReplacer(itemPos);
+            return this.Count == newCount && this.initList[itemPos].Equals(replacer);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.initList[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.initList[i];
+            }
         }
 
         private void SortWhileAdding(T element)
@@ -119,6 +168,16 @@ namespace ArrayLibrary
         private bool IsSmallerOrEqual(T first, T second)
         {
             return first.CompareTo(second) < 0 || first.CompareTo(second) == 0;
+        }
+
+        private T GetReplacer(int itemPos)
+        {
+            if (itemPos + 1 < this.Count)
+            {
+                return this.initList[itemPos + 1];
+            }
+
+            return this.initList[itemPos - 1];
         }
     }
 }
