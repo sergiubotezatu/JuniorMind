11a61c38768b4d70e6ebebd6bb13a962729760d9
Sergiu95 <Sergiu95@SERGIU>
2021-06-30 17:10:48 +0300

    Linq assesments

---
+++ Linq/Linq/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Linq;
+
+namespace Linq
+{
+    public static class Class1
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source.Equals(null) || predicate.Equals(null))
+            {
+                throw new ArgumentNullException($"{0}was null", source.Equals(null)? nameof(source) : nameof(predicate));
+            }
+            foreach(TSource element in source)
+            {
+                if (!predicate(element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
