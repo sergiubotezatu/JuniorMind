e708565d74d3ac44c7e06f2568a3ebb18b9d29f3
Sergiu95 <Sergiu95@SERGIU>
2021-07-05 16:20:42 +0300

    First

--- Linq/Linq/Class1.cs
+++ Linq/Linq/Class1.cs
@@ -21,12 +21,56 @@ namespace Linq
             return true;
         }
 
+        public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            ThrowIsNull(source);
+            ThrowIsNull(predicate);
+            foreach (TSource element in source)
+            {
+                if (predicate(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static TSource First<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            ThrowIsNull(source);
+            ThrowIsNull(predicate);
+            if (source.Count() == 0)
+            {
+                throw new InvalidOperationException("The source sequence is empty.");
+            }
+            foreach (TSource element in source)
+            {
+                if (predicate(element))
+                {
+                    return element;
+                }
+            }
+
+            throw new InvalidOperationException("There is no element in this collection that meets the correct criteria.");
+        }
+
         static void ThrowIsNull<T>(T toValidate)
         {
             if (toValidate.Equals(null))
             {
                 throw new ArgumentNullException($"{nameof(toValidate)}was null");
             }
+        }
+
+        static int Count<T>(this IEnumerable<T> elements)
+        {
+            int count = 0;
+
+            foreach (var element in elements)
+                count += 1;
+
+            return count;
         }
     }
 }
