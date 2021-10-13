b16491543ec33679f6c290eb9a3c350b332fba75
Sergiu95 <Sergiu95@SERGIU>
2021-10-13 13:14:21 +0300

    tupleCount

--- Diverse/Diverse/LetterCount.cs
+++ Diverse/Diverse/LetterCount.cs
@@ -6,24 +6,19 @@ namespace Diverse
 {
     public class LetterCount
     {
-        public string CountVowelsAndCons(string word)
+        public (int, int) CountVowelsAndCons(string word)
         {
             IEnumerable<char> characters = word.ToCharArray().AsEnumerable();
             var letters = characters.Where(x => char.IsLetter(x));
-            int vowelsNumber = letters.Aggregate(0, (x, y) => CountVowels(x, y));
-            return $"Your word contains {letters.Count() - vowelsNumber} consonants and {vowelsNumber} vowels";
+            int vowelsNumber = letters.Aggregate(0, (count, character) => IsVowel(character) ? count + 1 : count);
+            (int consonants, int vowels) result = ((letters.Count() - vowelsNumber), vowelsNumber);
+            return result;
         }
 
-        private int CountVowels(int count, char letter)
+        private bool IsVowel(char letter)
         {
-            IEnumerable<char> vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
-            if (vowels.Contains(letter))
-            {
-                count += 1;
-            }
-
-            return count;
-
+            string vowels = "aeiou";
+            return vowels.Contains(letter);
         }
     }
 }
