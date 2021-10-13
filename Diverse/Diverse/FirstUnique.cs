c85087b705ff1623e6f33c930974ecb76faefe59
Sergiu95 <Sergiu95@SERGIU>
2021-10-13 18:15:23 +0300

    FirstNonDuplicate

---
+++ Diverse/Diverse/FirstUnique.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace Diverse
+{
+    public class FirstUnique
+    {
+        private char GetOneOccurence(string input)
+        {
+            return input.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).FirstOrDefault();
+        }
+    }
+}
