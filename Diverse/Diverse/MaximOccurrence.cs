a48d3a9a09f64ea2f2ac48d96043087203d7c9ae
Sergiu95 <Sergiu95@SERGIU>
2021-10-15 18:40:20 +0300

    GetMaxOccurrence

---
+++ Diverse/Diverse/MaximOccurrence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Diverse
+{
+    class MaximOccurrence
+    {
+        public IEnumerable<char> GetMaxOccurence(string input)
+        {
+            return input.GroupBy(x => x).OrderByDescending(x => x.Count()).GroupBy(x => x.Count()).First().Take(1).Select(x => x.Key);
+        }
+    }
+}
