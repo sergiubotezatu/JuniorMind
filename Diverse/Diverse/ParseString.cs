f9d21b7d00064e9eec2af6c2fef6622d05d5524d
Sergiu95 <Sergiu95@SERGIU>
2021-10-15 14:51:16 +0300

    simpler converter

--- Diverse/Diverse/ParseString.cs
+++ Diverse/Diverse/ParseString.cs
@@ -11,11 +11,10 @@ namespace Diverse
         {
             result = 0;
             IEnumerable<char> toWorkWith = input;
-            Func<IEnumerable<char>, int, int> ToInteger = Convert;
-            if (input.First() == '-')
+            bool isNegative = input.First() == '-';
+            if (isNegative)
             {
                 toWorkWith = toWorkWith.Skip(1);
-                ToInteger = ConvertNegative;
             }
 
             if (!IsConvertable(toWorkWith))
@@ -23,18 +22,13 @@ namespace Diverse
                 return false;
             }
 
-            result = ToInteger(toWorkWith, toWorkWith.Count() - 1);
+            result = isNegative ? -1 * Convert(toWorkWith) : Convert(toWorkWith);
             return true;
         }
 
-        private int Convert(IEnumerable<char> input, int digits)
+        private int Convert(IEnumerable<char> input)
         {
-            return input.Select(x => ToTens(x, digits--)).Aggregate(0, (result, number) => result + number);
-        }
-
-        private int ConvertNegative(IEnumerable<char> toConvert, int digits)
-        {
-            return 0 - Convert(toConvert, digits);
+            return input.Select(x => ToUnit(x)).Aggregate(0, (result, number) => result * 10 + number);
         }
 
         private bool IsConvertable(IEnumerable<char> toCheck)
@@ -48,30 +42,9 @@ namespace Diverse
             return IsInRange(lead, ('1', '9')) && toCheck.Skip(1).All(x => IsInRange(x, ('0','9')));
         }       
 
-        private int ToTens(char toMultiply, int exponent)
-        {
-            if (toMultiply == '0')
-            {
-                return 0;
-            }
-
-            return ToUnit(toMultiply) * TenToThe(exponent);
-        }
-
         private int ToUnit(char toConvert)
         {
             return toConvert - '0';
-        }
-
-        private int TenToThe(int exponent)
-        {
-            int result = 1;
-            for (int i = 0; i < exponent; i++)
-            {
-                result *= 10;
-            }
-
-            return result;
         }
 
         private bool IsInRange(char toCheck, (char, char) range)
