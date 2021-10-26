8bc9747346ea6cfeb0f442162fb5a52ede987590
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2021-10-26 12:08:30 +0300

    getPalindrome

---
+++ Diverse/Diverse/PalindromeValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Diverse
+{
+    public class PalindromeValidator
+    {
+        public IEnumerable<string> GetPalindrome(string input)
+        {
+            int skipped = 0;
+            int selection = 1;
+            while(selection <= input.Length)
+            {
+                while(skipped + selection <= input.Length)
+                {
+                    string result = input.Skip(skipped).Take(selection).ToString();
+                    if (result.Equals(result.Reverse()))
+                    {
+                        yield return result;
+                    }
+
+                    skipped++;
+                }
+
+                skipped = 0;
+                selection++;
+            }
+        }
+    }
+}
