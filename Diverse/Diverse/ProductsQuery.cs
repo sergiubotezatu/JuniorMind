2079f540097f3c4ce8bf9e79f2615ce8e11b9a8b
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2021-11-05 17:08:06 +0200

    Product features

---
+++ Diverse/Diverse/ProductsQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Diverse
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public ICollection<Feature> Features { get; set; }
+    }
+
+    public class Feature
+    {
+        public int Id { get; set; }
+    }
+
+    class ProductsQuery
+    {
+        private ICollection<Product> products;
+        private ICollection<Feature> features;
+
+        public IEnumerable<Product> GetLeastOneFeature()
+        {
+            return products.Where(x => x.Features.Any(feat => features.Contains(feat)));
+        }
+
+        public IEnumerable<Product> GetProductsAllFeatures()
+        {
+            return products.Where(x => x.Features.All(feat => features.Contains(feat)));
+        }
+
+        public IEnumerable<Product> GetProductsWithNoFeatures()
+        {
+            return products.Except(GetLeastOneFeature());
+        }
+    }
+}
