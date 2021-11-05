d14d461481ae644e4783a1dc6f575de7071a51c0
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2021-11-05 17:54:26 +0200

    group by Qty

---
+++ Diverse/Diverse/ProductsQtySort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Diverse
+{
+    struct Product2
+    {
+        public string Name;
+        public int Quantity;
+    }
+
+    class ProductsQtySort
+    {
+        public IEnumerable<Product2> SortQuantityBased(List<Product2> first, List<Product2> second)
+        {
+            return first.GroupJoin(second, firstProds => firstProds.Name,
+                secondProds => secondProds.Name,
+                (product, productGroup) =>
+                new Product2 { Name = product.Name, Quantity = productGroup.Select(x => x.Quantity).Sum() });
+        }
+    }
+}
