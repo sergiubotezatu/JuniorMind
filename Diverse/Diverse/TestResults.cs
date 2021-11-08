dfbe24662540c53efa50b62e9f0bce66d58b33cc
sergiu.botezatu95@gmail.com <sergiu.botezatu95@gmail.com>
2021-11-08 15:18:26 +0200

    High scores

---
+++ Diverse/Diverse/TestResults.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Diverse
+{
+    public class TestResults
+    {
+        public string Id { get; set; }
+        public string FamilyId { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class FamHighScore
+    {
+        public IEnumerable<TestResults> GetHighScores(IEnumerable<TestResults> results)
+        {
+            return results.GroupBy(family => family.FamilyId).Select(x => x.OrderByDescending(criteria => criteria.Score).First());
+        }
+    }
+    
+}
